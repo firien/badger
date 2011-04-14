aedaab7db8941d4387f8845dd27e02fcdd6a8956
Patrick Griffin <halifirien@gmail.com>
2011-04-13 22:27:26 -0400

    new main function for no xib

--- main.m
+++ main.m
@@ -7,8 +7,18 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "badgerAppDelegate.h"
 
-int main(int argc, char *argv[])
-{
-    return NSApplicationMain(argc,  (const char **) argv);
+int main(int argc, char *argv[]) {
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  NSApplication *application = [NSApplication sharedApplication];
+
+  badgerAppDelegate *appDelegate = [[[badgerAppDelegate alloc] init] autorelease];
+
+  [application setDelegate:appDelegate];
+  [application run];
+
+  [pool drain];
+
+  return EXIT_SUCCESS;
 }
