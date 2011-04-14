dc22289293c1b00c4235923989187442b747c336
Patrick Griffin <halifirien@gmail.com>
2011-04-14 14:37:20 -0400

    code clean up, better comments

--- badgerAppDelegate.m
+++ badgerAppDelegate.m
@@ -140,22 +140,20 @@
 }
 
 - (void)generateBadge:(NSString *)title {
-  // insert code here...
   int pixelsWide = size;
   int pixelsHigh = size;
 
   const char *step = [title cStringUsingEncoding:NSUTF8StringEncoding];
-  
+
   int bitmapBytesPerRow = (pixelsWide * 4);
-  
+
   CGImageRef theCGImage = NULL;
   CGContextRef context = NULL;
-  
   CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-  
+
   // create the bitmap context
   context = CGBitmapContextCreate(NULL, pixelsWide, pixelsHigh, 8, bitmapBytesPerRow, colorSpace, kCGImageAlphaPremultipliedLast);
-  
+
   if (context != NULL) {
     CGFloat gradientLocations[4] = { 0.0, 0.4, 0.6, 1.0 };
     CGFloat colors[16] = {
@@ -164,85 +162,74 @@
       0.0, 0.0, 0.0, 0.1,
       0.0, 0.0, 0.0, 0.0
     };
-    
-    // create the CGGradient and then release the gray color space
+
+    // create the CGGradient
     CGGradientRef gradient = CGGradientCreateWithColorComponents(colorSpace, colors, gradientLocations, 4);
-    
-    int lineWidth = floor(pixelsHigh / 10);//this should be an even number
+
+    int lineWidth = floor(pixelsHigh / 10);
     if ((lineWidth % 2) != 0) {
-      lineWidth++;
+      lineWidth++;//ensure this is an even number
     }
     int padding = floor(pixelsHigh / 11);
     int inset = floor(lineWidth / 2) + padding;
     int diameter = pixelsWide - lineWidth - (padding * 2);
     CGRect circle = CGRectMake(inset, inset, diameter, diameter);
-    // create the start and end points for the gradient vector (straight down)
-    CGContextSaveGState(context);
-    //shadow
+
+    // draw border with shadow
     CGFloat shadowColorValues[4] = {0.4, 0.4, 0.4, 0.9};
     CGColorRef shadowColor = CGColorCreate(colorSpace, shadowColorValues);
     CGContextSetShadowWithColor(context, CGSizeMake(0.0, 0.0), floor(pixelsHigh / 9), shadowColor);
     CGContextSetLineWidth(context, lineWidth);
-    CGContextAddEllipseInRect(context, circle);
-    CGContextStrokePath(context);
-    CGColorRelease(shadowColor);
-    
-    // draw the gradient into the gray bitmap context
-    CGContextBeginPath(context);
-    CGContextAddEllipseInRect(context, circle);
-    CGContextClip(context);
-    //fill with selected color
-    NSArray *colorArray = [self colorArray];
-    CGContextSetRGBFillColor(context, [[colorArray objectAtIndex:0] floatValue], [[colorArray objectAtIndex:1] floatValue], [[colorArray objectAtIndex:2] floatValue], 1.0);
-    CGContextFillRect(context, CGRectMake(0,0, pixelsWide, pixelsHigh));
-    CGPoint center = CGPointMake(pixelsWide / 2, 0);
-    CGContextDrawRadialGradient(context, gradient, center, 0, center, pixelsHigh, kCGGradientDrawsAfterEndLocation);
-    CGContextRestoreGState(context);
-    
-    CGContextSetLineWidth(context, pixelsHigh / 10);
     CGContextSetRGBStrokeColor(context, 1.0, 1.0, 1.0, 1.0);
     CGContextAddEllipseInRect(context, circle);
     CGContextStrokePath(context);
-    
+    CGColorRelease(shadowColor);
+
+    // fill with selected color
+    CGContextBeginPath(context);
+    CGContextAddEllipseInRect(context, circle);
+    CGContextClip(context);
+    NSArray *colorArray = [self colorArray];
+    CGContextSetRGBFillColor(context, [[colorArray objectAtIndex:0] floatValue], [[colorArray objectAtIndex:1] floatValue], [[colorArray objectAtIndex:2] floatValue], 1.0);
+    CGContextFillRect(context, CGRectMake(0,0, pixelsWide, pixelsHigh));
+    // draw the gradient
+    CGPoint center = CGPointMake(pixelsWide / 2, 0);
+    CGContextDrawRadialGradient(context, gradient, center, 0, center, pixelsHigh, kCGGradientDrawsAfterEndLocation);
+    // clean up the gradient
+    CGGradientRelease(gradient);
+
+    // draw text
     CGContextSetRGBFillColor(context, 1.0, 1.0, 1.0, 1.0);
     CGContextSetRGBStrokeColor(context, 1.0, 0.0, 0.0, 1.0);
     CGContextSetShadow(context, CGSizeMake(0.0, -3.0), 2.0);
     CGContextSelectFont(context, "Helvetica-Bold", floor(pixelsHigh / 1.5), kCGEncodingMacRoman);
-    // Next we set the text matrix to flip our text upside down. We do this because the context itself
-    // is flipped upside down relative to the expected orientation for drawing text (much like the case for drawing Images & PDF).
-    CGContextSetTextMatrix(context, CGAffineTransformMakeScale(1.0, 1.0));
-    
     //determine length of text first
     CGPoint startText = CGContextGetTextPosition(context);
     CGContextSetTextDrawingMode(context, kCGTextInvisible);
     CGContextShowText(context, step, strlen(step));
     CGPoint endText = CGContextGetTextPosition(context);
-    
     CGContextSetTextDrawingMode(context, kCGTextFill);
     CGContextShowTextAtPoint(context, (pixelsWide / 2.f) - ((endText.x - startText.x) / 2.f), floor(pixelsHigh / 3.5), step, strlen(step));
-    
-    // clean up the gradient
-    CGGradientRelease(gradient);
-    
+
     // convert the context into a CGImageRef and release the context
     theCGImage = CGBitmapContextCreateImage(context);
     CGContextRelease(context);
-    
+
     OSStatus err = noErr;
     PasteboardRef theClipboard;
-    
+
     err = PasteboardCreate(kPasteboardClipboard, &theClipboard);
     err = PasteboardClear(theClipboard);
-    
+
     CFMutableDataRef url = CFDataCreateMutable(kCFAllocatorDefault, 0);
-    
+
     CFStringRef type = kUTTypePNG;
     size_t count = 1;
     CFDictionaryRef options = NULL;
     CGImageDestinationRef dest = CGImageDestinationCreateWithData(url, type, count, options);
     CGImageDestinationAddImage(dest, theCGImage, NULL);
     CGImageDestinationFinalize(dest);
-    
+
     err = PasteboardPutItemFlavor(theClipboard, (PasteboardItemID)1, type, url, 0);
     CFRelease(url);
     CFRelease(dest);
